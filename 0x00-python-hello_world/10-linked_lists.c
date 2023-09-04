dd857e84883de002108e1ad68c18ee3185655bb9
alibayomy <alibayomy12@gmail.com>
2023-09-04 11:53:33 -0700

    adding the check_cycle function in seperated file

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -61,22 +61,3 @@ void free_listint(listint_t *head)
 		free(current);
 	}
 }
-/**
- * check_cycle - checks if the given listint_t list is a cycle list
- * @list: a pointer to a given listint_t list
- * Return: 1 if the given listint_t is a cycle, 0 if there is no cycle
- */
-int check_cycle(listint_t *list)
-{
-	listint_t *tmp;
-	listint_t *head = list;
-
-	tmp = list;
-	while (head)
-	{
-		if (head->next  == tmp)
-			return (1);
-		head = head->next;
-	}
-	return (0);
-}
