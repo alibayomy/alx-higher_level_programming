024ee50da7c75eb1963a239a118f95f45f0828b8
alibayomy <alibayomy12@gmail.com>
2023-09-04 13:40:15 -0700

    fixing the while condintion

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,7 +8,7 @@ int check_cycle(listint_t *list)
 {	listint_t *slow = list;
 	listint_t *fast = list;
 
-	while (fast->next->next != NULL)
+	while (fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
