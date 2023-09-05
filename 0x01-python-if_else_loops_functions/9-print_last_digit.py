5f7166afb19c0b2d54404d1986a2513570a84341
alibayomy <alibayomy12@gmail.com>
2023-09-05 11:53:25 -0700

    testing file num 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_digit = abs(number) % 10
-    if (number < 0):
-        print(-last_digit)
-    else:
-        print(last_digit)
+    print(last_digit,end="")
+    return (last_digit)
