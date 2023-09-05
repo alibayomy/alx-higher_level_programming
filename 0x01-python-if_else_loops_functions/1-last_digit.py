3a72559fc401ff8fe650589711c6c11da93d9a4c
alibayomy <alibayomy12@gmail.com>
2023-09-05 10:10:24 -0700

    fixing the style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-
-if elif number < 0:
-    print("Last digit of", number, "is", -last_digit,"and is less than 6 and not 0") 
+l_6 = "and is less than 6 and not 0"
+g_5 = "and is greater than 5"
+if number < 0:
+    print("Last digit of", number, "is", -last_digit, l_6)
 elif last_digit > 5:
-    print("Last digit of", number, "is", last_digit, 
-            "and is greater than 5")
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
 elif last_digit == 0:
     print("Last digit of", number, "is", last_digit, "and is 0")
 else:
-    print("Last digit of", number, "is", -last_digit, 
-            "and is less than 6 and not 0")
+    print("Last digit of", number, "is", -last_digit, "{}", l_6)
