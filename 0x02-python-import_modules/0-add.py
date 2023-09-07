f9cb854e9ca47849edf2b42463fc013a58d13c05
alibayomy <alibayomy12@gmail.com>
2023-09-07 07:00:26 -0700

    adjusting the pycodestyle

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("{}".format(a), "+", "{}".format(b), "=", add(a,b))
+    print("{}".format(a), "+", "{}".format(b), "=", add(a, b))
