40cf68541cfd5a7b187b2901d0c3d92b0b502993
alibayomy <alibayomy12@gmail.com>
2023-09-07 12:11:31 -0700

    testing file num 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
+    index = 1
+    n = n - 1
+    if (n == 1):
+        print(n, "argument:")
+    elif n == 0:
+        print(n, "arguments.")
+    else:
+        print(n, "arguments:")
+    for i in range(n):
+        print("{}: {}".format(i + 1, sys.argv[i]))
