33be44316e4ac511310be29bb90dfb84649621d1
alibayomy <alibayomy12@gmail.com>
2023-09-10 09:12:20 -0700

    adding the {:d} in file num 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     n = len(my_list)
     for i in range(n):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
