323b129b82973c291e3642e2890d158df6ea2c34
alibayomy <alibayomy12@gmail.com>
2023-09-10 09:32:04 -0700

    adding the {:d} file num 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for lst in matrix:
         for n in lst:
             if (n == len(lst)):
-                print("{}".format(n), end="")
+                print("{:d}".format(n), end="")
                 break
-            print("{} ".format(n), end="")
+            print("{:d} ".format(n), end="")
         print()
