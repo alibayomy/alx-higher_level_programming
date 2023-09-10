52d8414207436492c326ef2389f9d22e91c27d08
alibayomy <alibayomy12@gmail.com>
2023-09-10 09:16:14 -0700

    checking if the given element is a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    n = len(my_list)
-    for i in range(n):
-        print("{:d}".format(my_list[i]))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        n = len(my_list)
+        for i in range(n):
+            print("{:d}".format(my_list[i]))
