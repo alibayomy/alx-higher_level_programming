af27ad440dd476605ef8024ece632827752e7488
alibayomy <alibayomy12@gmail.com>
2023-09-10 13:24:26 -0700

    fixing file num 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for lst in matrix:
         for n in lst:
-            if (n == len(lst)):
-                print("{:d}".format(n), end="")
-                break
-            print("{:d} ".format(n), end="")
+            if (n == len(lst) - 1):
+                print("{:d}".format(n), end=" ")
+            else:
+                print("{:d} ".format(n), end="")
         print()
