1d0333fd4311492b91992d4f70dff96bf127111f
alibayomy <alibayomy12@gmail.com>
2023-09-10 09:18:10 -0700

    fixing the if condintion in file num 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     n = len(new_list)
-    if idx < 0 or idx > (n + 1):
+    if idx < 0 or idx > (n - 1):
         return new_list
     new_list[idx] = element
     return new_list
