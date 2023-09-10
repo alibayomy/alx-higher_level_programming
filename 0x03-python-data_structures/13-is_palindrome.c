50806aae1f1b80b4ebe54d0be93486b19f69adac
alibayomy <alibayomy12@gmail.com>
2023-09-10 12:57:25 -0700

    testing file num 12 the interview one

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * is_palindrome - check if the given linked list is a palindrome
+ * @head: a pointer to the pointer of the head of the linked list
+ * Return: 1 if it is a palindrome, else 0
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t *first;
+	listint_t *last;
+	int len = 0, distance, move;
+	
+	last = *head;
+	first = *head;
+	while (last)
+	{
+		len++;
+		last = last->next;
+	}
+	distance = len;
+	last = *head;
+	distance--;
+	while (distance > 0)
+	{
+		move = 0;
+		while (move < distance)
+		{
+			move++;
+			last = last->next;
+		}
+		if (first->n == last->n)
+		{
+			first = first->next;
+			last = *head;
+			distance = distance - 1;
+		}
+		else
+			return (0);
+	}
+	return (1);
+}
