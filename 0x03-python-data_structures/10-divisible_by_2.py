677d73a0e0a6b172b21a2d3b3ff449b39393ca55
alibayomy <alibayomy12@gmail.com>
2023-09-11 09:16:21 -0700

    fixing file num 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_lst = []
     for i in my_list:
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             new_lst.append(True)
         else:
             new_lst.append(False)
