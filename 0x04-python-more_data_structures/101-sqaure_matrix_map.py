9d752dce1e7b133dd1d27b609089720383952244
alibayomy <alibayomy12@gmail.com>
2023-09-13 12:38:25 -0700

    testing file num 101

---
+++ 0x04-python-more_data_structures/101-sqaure_matrix_map.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_lst = matrix[:]
+    for i in range(len(matrix)):
+        new_lst[i] = list(map(lambda x: x*x, matrix[i]))
+    return list(new_lst) 
