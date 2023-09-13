3e16465fa78c71867847f842058ce214a731aafb
alibayomy <alibayomy12@gmail.com>
2023-09-13 10:38:23 -0700

    testing file num 2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_lst = matrix[:]
-    double = lambda x : x * x
     for i in range(len(matrix)):
-        new_lst[i] = list(map(double, matrix[i]))
+        new_lst[i] = list(map(lambda x: x+x, matrix[i]))
     return list(new_lst)
