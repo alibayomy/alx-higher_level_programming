d193e8d46d440a5cc9c03d14245f194192fbb4b7
alibayomy <alibayomy12@gmail.com>
2023-09-13 10:29:28 -0700

    adjusting to not modify the main matrixc

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_lst = matrix
+    new_lst = matrix[:]
     double = lambda x : x * x
     for i in range(len(matrix)):
         new_lst[i] = list(map(double, matrix[i]))
