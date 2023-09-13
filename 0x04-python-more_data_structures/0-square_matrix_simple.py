048669f6998aca919212701cf54743c72b39d155
alibayomy <alibayomy12@gmail.com>
2023-09-13 10:26:06 -0700

    testing file num 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_lst = matrix
+    double = lambda x : x * x
+    for i in range(len(matrix)):
+        new_lst[i] = list(map(double, matrix[i]))
+    return list(new_lst)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+ thif file contain my answers for 0x04 more data strcutre in ALX month 4
