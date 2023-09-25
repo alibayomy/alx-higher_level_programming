a32e02341a27f710fc1ef1a4a1247d57904071c1
alibayomy <alibayomy12@gmail.com>
2023-09-25 11:22:33 -0700

    remvoing the extra append

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,6 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(0, list_length):
         try:
             rslt = my_list_1[i] / my_list_2[i]
-            new_lst.append(rslt)
         except TypeError:
             rslt = 0
             print("wrong type")
