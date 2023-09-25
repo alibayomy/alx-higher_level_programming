3833d32369a572ff7b527876594a1e980a4ad1f7
alibayomy <alibayomy12@gmail.com>
2023-09-25 10:52:45 -0700

    testing with continue

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             num_printed = num_printed + 1
             num = num + 1
         except (TypeError, ValueError):
-            print("", end="")
             num = num + 1
+            continue
     print()
     return (num_printed)
