19fc05a439f010ca691780e61a797727136477c7
alibayomy <alibayomy12@gmail.com>
2023-09-25 11:21:03 -0700

    adding the finally part

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,12 +6,14 @@ def list_division(my_list_1, my_list_2, list_length):
             rslt = my_list_1[i] / my_list_2[i]
             new_lst.append(rslt)
         except TypeError:
-            new_lst.append(0)
+            rslt = 0
             print("wrong type")
         except ZeroDivisionError:
-            new_lst.append(0)
+            rslt = 0
             print("division by 0")
         except IndexError:
-            new_lst.append(0)
+            rslt = 0
             print("out of range")
+        finally:
+            new_lst.append(rslt)
     return new_lst
