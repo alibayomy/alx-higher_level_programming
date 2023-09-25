5af4026920baa77b863247359b4e4ced53dab1f1
alibayomy <alibayomy12@gmail.com>
2023-09-25 10:56:48 -0700

    testing the answer with for loop

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     num_printed = 0
     num = 0
-    while (num_printed < x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[num]), end="")
             num_printed = num_printed + 1
-            num = num + 1
         except (TypeError, ValueError):
-            num = num + 1
             continue
     print("")
     return (num_printed)
