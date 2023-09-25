b2a98bb2059cae52f12df030b00037d6083ce201
alibayomy <alibayomy12@gmail.com>
2023-09-25 10:51:02 -0700

    testing file num 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num_printed = 0
+    num = 0
+    while (num_printed < x):
+        try:
+            print("{:d}".format(my_list[num]), end="")
+            num_printed = num_printed + 1
+            num = num + 1
+        except (TypeError, ValueError):
+            print("", end="")
+            num = num + 1
+    print()
+    return (num_printed)
