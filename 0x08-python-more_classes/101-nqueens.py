3a67badda7a06eb80e3cdbf7711ad709f6035daa
alibayomy <alibayomy12@gmail.com>
2023-10-02 13:16:00 -0700

    testing

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""The Nqueens Buzzle"""
+
+
+import sys
+
+
+def solution(int):
+    """till i know the solution"""
+    pass
+
+
+if __name__ == "__main__":
+    if len((sys.argv)) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is false:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
