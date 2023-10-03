153afe5e852f3882bc89372df113ef09da51df53
alibayomy <alibayomy12@gmail.com>
2023-10-03 12:02:23 -0700

    testing 103 and 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,9 +3,9 @@
 
 
 class LockedClass(object):
-    """no class or object attribute, 
-    that prevents the user from dynamically 
+    """no class or object attribute,
+    that prevents the user from dynamically
     creating new instance attributes, except
     if the new instance attribute is called first_name."""
-    
+
     __slots__ = 'first_name'
