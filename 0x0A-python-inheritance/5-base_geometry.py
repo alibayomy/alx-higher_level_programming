09c4d0f79131da10a6b65c03f7f6700e043e02a0
alibayomy <alibayomy12@gmail.com>
2023-10-09 12:28:48 -0700

    testing files 5 and 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """My own definied BaseGeometry """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """My own definied BaseGeometry """
+    def area(self):
+        raise Exception("area() is not implemented")
