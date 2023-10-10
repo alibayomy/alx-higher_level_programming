8b5431b3583bd3e566f8acd790a58c9a8ab14326
alibayomy <alibayomy12@gmail.com>
2023-10-10 11:04:19 -0700

    not printing a new line after print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """a function that reads a text file (UTF8)
                 and prints it to stdout"""
     with open(filename, mode="r", encoding="utf-8") as myFile:
-        print(myFile.read())
+        print(myFile.read(), end="")
