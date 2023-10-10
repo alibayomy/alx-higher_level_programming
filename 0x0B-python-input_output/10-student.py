69a4f9928eef00dc63da2708297acb3984456ead
alibayomy <alibayomy12@gmail.com>
2023-10-10 12:51:25 -0700

    testing file num 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student  class"""
+
+
+class Student:
+    """a class Student that defines a student by:
+
+            Public instance attributes:
+            first_name
+            last_name
+            age"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
