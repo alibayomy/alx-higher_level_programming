2da0d71f6dbd55151b0b099f3154afa33746f693
alibayomy <alibayomy12@gmail.com>
2023-10-10 10:57:44 -0700

    testing file num 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Read file function"""
+
+
+def read_file(filename=""):
+    """a function that reads a text file (UTF8)
+                and prints it to stdout"""
+    with open(filename, mode="r", encoding="utf-8") as myFile:
+        print(myFile.read())
