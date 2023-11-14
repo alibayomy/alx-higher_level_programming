8e351714414c834b2558b7c4bae0df46b52a1254
alibayomy <alibayomy12@gmail.com>
2023-11-14 13:33:26 -0800

    UPPERCASE FROM

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Displays the number of the records with id = 89 in the table first_table of the databas in My SQL server
-SELECT COUNT(*) from first_table WHERE id=89;
+SELECT COUNT(*) FROM first_table WHERE id=89;
