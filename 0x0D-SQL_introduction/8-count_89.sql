7bf8bd14f6ec6df3f7a8d0fa0d2a00c744a99d10
alibayomy <alibayomy12@gmail.com>
2023-11-14 13:31:08 -0800

    testing file num 8 again

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Displays the number of the records with id = 89 in the table first_table of the databas in My SQL server
-SELECT COUNT(*) from first_table HAVING id = 89;
+SELECT COUNT(*) from first_table HAVING id=89;
