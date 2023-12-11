bdca0fa2cc51fd19cd27f54cccc4345db40d8b48
alibayomy <alibayomy12@gmail.com>
2023-12-11 08:04:47 -0800

    adding the <0 case

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,11 @@ const num = process.argv[2];
 if (num === undefined) {
   console.log('Missing size');
 } else {
-  let i = 0;
-  while (i < num) {
-    console.log('X'.repeat(num));
-    i += 1;
+  if (num > 0) {
+    let i = 0;
+    while (i < num) {
+      console.log('X'.repeat(num));
+      i += 1;
+    }
   }
 }
