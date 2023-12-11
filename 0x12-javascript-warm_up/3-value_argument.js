cacdccddb89411049458436b00cb8cac23b62c71
alibayomy <alibayomy12@gmail.com>
2023-12-11 07:41:14 -0800

    testing file num 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,5 @@ const args = process.argv;
 if (args[2] === undefined) {
   console.log('No argument');
 } else {
-  for (let i = 2; i < args.length; i++) {
-    console.log(args[i]);
-  }
+  console.log(args[2]);
 }
