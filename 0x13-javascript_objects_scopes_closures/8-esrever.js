d18e3c9a1249d9f699b9a9ca496c81cd568061d3
alibayomy <alibayomy12@gmail.com>
2023-12-12 08:51:52 -0800

    adjusting the style for file num 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    let newLst = [];
-    for (let i = list.length -1; i >=0; i--)
-        newLst.push(list[i]);
-    return newLst
-}
+  const newLst = [];
+  for (let i = list.length - 1; i >= 0; i--) { newLst.push(list[i]); }
+  return newLst;
+};
