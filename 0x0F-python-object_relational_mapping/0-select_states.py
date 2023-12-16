d4e11488df1e182d85c2dcf118a4db653c3aa2dc
alibayomy <alibayomy12@gmail.com>
2023-12-16 11:20:30 -0800

    adding the if case

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,13 +3,13 @@
 import MySQLdb
 import sys
 
-
-dp = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                        password=sys.argv[2], database=sys.argv[3], port=3306)
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    dp = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                            password=sys.argv[2], database=sys.argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
