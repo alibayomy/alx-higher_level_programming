9b5edfbf27b2c9da2ceb7bd0473fc418a88c4d93
alibayomy <alibayomy12@gmail.com>
2024-01-07 15:40:21 -0800

    retesting file num 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""A script that:
-- takes a URL,
-- makes a POST request to it,
-- takes the email as an argument,
-- shows the body of the response
-"""
+"""a Python script that takes in a URL and an email,
+    sends a POST request to the passed URL with the email as a parameter,
+    and displays the body of the response (decoded in utf-8)"""
 
-import urllib.parse
-import urllib.request
-import sys
 
 if __name__ == '__main__':
-    url = sys.argv[1]  # Get the URL from command line argument
-    email = sys.argv[2]  # Get the email from command line argument
-
-    data = urllib.parse.urlencode({'email': email}).encode(
-        'utf-8')  # Encode the email as URL parameter
-
-    with urllib.request.urlopen(url, data) as response:
-        # Read and decode the body of the response
-        body = response.read().decode('utf-8')
-
-    print(body)
+    import sys
+    import urllib.request
+    import urllib.parse
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode("utf-8")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        myPage = response.read()
+        print(myPage.decode("utf-8"))
