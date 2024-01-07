3cd46fec6b0619f106b056bcb6603f725637f385
alibayomy <alibayomy12@gmail.com>
2024-01-07 15:20:03 -0800

    retesting file num 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""A script that fetches from url:
-- https://alx-intranet.hbtn.io/status.
-- using urlib package.
-"""
+"""Write a Python script that
+    fetches https://alx-intranet.hbtn.io/status"""
 
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as page:
+        pageContent = page.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(pageContent)))
+        print("\t- content: {}".format(pageContent))
+        print("\t- utf8 content: {}".format(pageContent.decode("UTF-8")))
