b4bb6b37a3d79c3ec656b023b280d9082f2904fd
alibayomy <alibayomy12@gmail.com>
2024-01-07 15:51:23 -0800

    adjusting the syntax

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     import sys
     import urllib.request
     import urllib.error
-    url = sys.argv[1]
     try:
+        url = sys.argv[1]
         with urllib.request.urlopen(url) as response:
             print(response.read().decode("utf-8"))
-    except urllib.error.HTTPError  as e:
-        print("Error code: {}".foramt(e.code))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
