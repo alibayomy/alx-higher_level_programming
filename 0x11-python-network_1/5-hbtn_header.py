1fcabc29397679b33aaabf5c11ca21a6062b9402
alibayomy <alibayomy12@gmail.com>
2024-01-07 15:59:16 -0800

    re testing file nu m5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""
-Script that takes in a URL, sends a request to the URL and displays the
-value of the variable X-Request-Id in the response header.
-
-Usage: ./5-hbtn_header.py <URL>
-"""
-import requests
-from sys import argv
+"""a Python script that takes in a URL, 
+    sends a request to the URL and displays 
+    the value of the variable X-Request-Id in the response header"""
 
 
-if __name__ == "__main__":
-    url = argv[1]
-    req = requests.get(url)
+if __name__ == '__main__':
+    import requests
 
-    print(req.headers.get("X-Request-Id"))
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    headers = r.headers
+    print(headers['X-Request-Id'])
