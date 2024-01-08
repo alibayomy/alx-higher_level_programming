6977f17d8821e05a0335d69cbc3d09e9e2d1ceba
alibayomy <alibayomy12@gmail.com>
2024-01-07 16:17:08 -0800

    retesting file nu 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""
-Script that takes in a URL, sends a request to the URL and displays
-the body of the response.
+"""a Python script that takes in a URL,
+    sends a request to the URL and displays
+    the body of the response."""
 
-Usage: ./7-error_code.py <URL>
-  - Handles HTTP errors.
-"""
-from sys import argv
+
+import sys
 import requests
-
-
-if __name__ == "__main__":
-    url = argv[1]
-    req = requests.get(url)
-
-    if req.status_code >= 400:
-        print("Error code: {}".format(req.status_code))
+if __name__ == '__main__':
+    url = sys.argv[1]
+    r = requests(url)
+    if (r.status_cide >= 400):
+        print("Error code: {}".format(r.status_code))
     else:
-        print(req.text)
+        print(r.text)
