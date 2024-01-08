5b00e728fbc9ecc43f36ffb0247fe0b651de5561
alibayomy <alibayomy12@gmail.com>
2024-01-07 16:23:11 -0800

    syntax error adjusting  file 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ import requests
 if __name__ == '__main__':
     url = sys.argv[1]
     r = requests.get(url)
-    if (r.status_cide >= 400):
+    if (r.status_code >= 400):
         print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
