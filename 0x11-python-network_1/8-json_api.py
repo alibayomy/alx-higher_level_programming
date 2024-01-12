3acb518e320c5ccf39c19b40bfe330d17d164d0b
alibayomy <alibayomy12@gmail.com>
2024-01-11 16:57:09 -0800

    adjusting the response varaiable

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     try:
         response = req.json()
         if(response != {}):
-            print("{} {}".format(req.get("id"), respnse.get("name")))
+            print("{} {}".format(response.get("id"), respnse.get("name")))
         else:
             print("No result")
     except ValueError:
