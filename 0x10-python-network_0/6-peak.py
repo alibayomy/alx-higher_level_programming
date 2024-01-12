7f93de996c2a045dce0d4c26ad7557cafdf0530b
alibayomy <alibayomy12@gmail.com>
2024-01-11 16:53:56 -0800

    testing this algo

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,14 +4,40 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
-    listLen = len(list_of_integers)
-    if list_of_integers == []:
+    if (list_of_integers == []):
         return None
-    elif (listLen == 1):
-        return list_of_integers[0]
+    left = leftTree(list_of_integers)
+    right = rightTree(list_of_integers)
+    if (left < right):
+        return right
+    return left
+
+
+def leftTree(numList):
+    """Left tree """
+    if (len(numList) == 0):
+        return 0
+    elif (len(numList) == 1):
+        return numList[0]
     else:
-        for i in range(1, listLen):
-            if (list_of_integers[i - 1] < list_of_integers[i]
-                    & list_of_integers[i] > list_of_integers[i + 1]):
-                return list_of_integers[i]
-        return list_of_integers[listLen - 1]
+        mid = (len(numList) - 1) // 2
+        if (numList[mid - 1] < numList[mid] &
+                numList[mid] > numList[mid+1]):
+            return numList[mid]
+        else:
+            return leftTree(numList[:mid + 1])
+
+
+def rightTree(numList):
+    """check right tree"""
+    if (len(numList) == 0) | (numList == []):
+        return 0
+    elif (len(numList) == 1):
+        return numList[0]
+    else:
+        mid = (len(numList)) // 2
+        if (numList[mid - 1] < numList[mid] &
+                numList[mid] > numList[mid+1]):
+            return numList[mid]
+        else:
+            return rightTree(numList[mid:])
