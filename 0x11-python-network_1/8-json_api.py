eb1d89a63424d130713b108e5f838172db0926ff
alibayomy <alibayomy12@gmail.com>
2024-01-11 17:02:03 -0800

    testing again

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
         values = {'q': sys.argv[1]}
     req = requests.post("http://0.0.0.0:5000/search_user", values)
     try:
-        response = req.json()
-        if(response != {}):
-            print("{} {}".format(response.get("id"), respnse.get("name")))
-        else:
+        response = r.json()
+        if response == {}:
             print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
